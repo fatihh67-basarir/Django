3010f50075cbc5f4ec9632b0b2bd4f92037394ef
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-23 15:15:40 +0300

    django

---
+++ firstapp/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+
+from .views import home
+# from fscohortApp.views import fshome
+
+
+urlpatterns = [
+    path('',home),
+    
+    
+
+]
---
+++ russelApp/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+
+from .views import russel
+# from fscohortApp.views import fshome
+
+
+urlpatterns = [
+    path('russel',russel),
+    
+    
+
+]
